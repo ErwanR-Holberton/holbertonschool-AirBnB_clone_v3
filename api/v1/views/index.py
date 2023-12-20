3b56ec374fd7df397f5f7e8608038dd1c391c4d9
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-20 14:49:18 +0100

    added description

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""defines endpoints and their output """
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
 
 @app_views.route('/api/v1/stats', strict_slashes=False)
 def get_stats():
+    """the client gets the status if the server is reachable and working"""
     stats = {}
     types_to_count = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
     for obj_type in types_to_count:
