c022d758d7715984ea563665c960935613b911af
PereDeMacron <peredemacron@gmail.com>
2023-12-20 14:34:37 +0100

    task 5 without comment

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify
 
-@app_views.route('/status', strict_slashes=False)
-def get_status():
-    return {"status": "OK"}
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def get_stats():
+    stats = {}
+    types_to_count = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
+    for obj_type in types_to_count:
+        count = storage.count(obj_type)
+        stats[obj_type] = count
+    return stats
