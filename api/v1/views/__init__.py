1fbd58a087143b95318bf6936b7373f95cd3e93a
PereDeMacron <peredemacron@gmail.com>
2023-12-21 14:30:23 +0100

    task 9

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """import thing here to avoid circular import"""
 from flask import Blueprint
-
+from .cities import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 if True:
     from api.v1.views.index import *
     from .states import *
-    from .cities import *
+    
     from .amenities import *
