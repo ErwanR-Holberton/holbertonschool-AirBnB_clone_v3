c0a97c012bcec36bc6d8453f20c47caa5564f2df
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-22 13:10:35 +0100

    Changed import order

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """view for place"""
-from api.v1.views import app_views
-from models import storage
-from models.place import Place
 from models.city import City
 from flask import jsonify, abort, request
+from models import storage
+from models.place import Place
+from api.v1.views import app_views
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
