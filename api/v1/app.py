f8903c2301ad16114b7bedd46e18b8ef02bc3f84
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-22 14:01:57 +0100

    Changed import order

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
 from flask import Flask
-from models import storage
-from api.v1.views import app_views
-import os
 from flask_cors import CORS
+from models import storage
+import os
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
