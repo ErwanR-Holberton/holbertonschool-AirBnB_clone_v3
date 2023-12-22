dbe3283163e334637b2f5b84c98d1903107a15eb
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-22 13:14:12 +0100

    Changed import order

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """view for review"""
-from api.v1.views import app_views
-from models import storage
-from models.review import Review
-from models.place import Place
-from models.user import User
 from flask import jsonify, abort, request
+from models import storage
+from models.place import Place
+from models.review import Review
+from models.user import User
+from api.v1.views import app_views
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'],
