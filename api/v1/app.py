21d2394eccb11e87c7d1bca70f08184ccefaedb5
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-22 14:03:46 +0100

    Fixed Pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,12 @@ from models import storage
 import os
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
