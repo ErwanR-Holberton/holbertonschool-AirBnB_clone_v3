f62a817d7f1868f0cce431a3c4cc5fe1ab943ecd
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-22 14:24:29 +0100

    Changed function order

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """view for amenities"""
-from flask import jsonify, abort, request
-from api.v1.views import app_views
-from models import storage
 from models.amenity import Amenity
+from flask import jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
 
 
 @app_views.route("/amenities", methods=["GET"], strict_slashes=False)
@@ -12,6 +12,34 @@ def amenity_get_all():
     amenities = storage.all(Amenity).values()
     amenities_list = [amenity.to_dict() for amenity in amenities]
     return jsonify(amenities_list)
+
+
+@app_views.route("/amenities/<amenity_id>", methods=["GET"],
+                 strict_slashes=False)
+def amenity_by_id(amenity_id):
+    """get an amenity"""
+
+    fetched_obj = storage.get("Amenity", str(amenity_id))
+
+    if fetched_obj is None:
+        abort(404)
+
+    return jsonify(fetched_obj.to_dict())
+
+
+@app_views.route("/amenities/<amenity_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def amenity_delete_by_id(amenity_id):
+
+    fetched_obj = storage.get("Amenity", str(amenity_id))
+
+    if fetched_obj is None:
+        abort(404)
+
+    storage.delete(fetched_obj)
+    storage.save()
+
+    return jsonify({}), 200
 
 
 @app_views.route("/amenities", methods=["POST"], strict_slashes=False)
@@ -32,19 +60,6 @@ def amenity_create():
     return jsonify(new_am.to_dict()), 201
 
 
-@app_views.route("/amenities/<amenity_id>", methods=["GET"],
-                 strict_slashes=False)
-def amenity_by_id(amenity_id):
-    """get an amenity"""
-
-    fetched_obj = storage.get("Amenity", str(amenity_id))
-
-    if fetched_obj is None:
-        abort(404)
-
-    return jsonify(fetched_obj.to_dict())
-
-
 @app_views.route("/amenities/<amenity_id>", methods=["PUT"],
                  strict_slashes=False)
 def amenity_put(amenity_id):
@@ -58,19 +73,4 @@ def amenity_put(amenity_id):
         if key not in ["id", "created_at", "updated_at"]:
             setattr(fetched_obj, key, val)
     fetched_obj.save()
-    return jsonify(fetched_obj.to_dict()); 200
-
-
-@app_views.route("/amenities/<amenity_id>", methods=["DELETE"],
-                 strict_slashes=False)
-def amenity_delete_by_id(amenity_id):
-
-    fetched_obj = storage.get("Amenity", str(amenity_id))
-
-    if fetched_obj is None:
-        abort(404)
-
-    storage.delete(fetched_obj)
-    storage.save()
-
-    return jsonify({}), 200
+    return jsonify(fetched_obj.to_dict()), 200
